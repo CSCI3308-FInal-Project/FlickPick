a7b5cbe83bedc5f6746eb481f34d8da72574d542
Matthew Casazza <mattcasazza14@gmail.com>
2026-04-02 14:29:46 -0600

    Lab 10 base

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -1,6 +1,8 @@
-const chai     = require('chai');
+const server = require('../src/index');
+
+const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app      = require('../src/index');
+const app = require('../src/index');
 
 chai.use(chaiHttp);
 const { expect } = chai;
